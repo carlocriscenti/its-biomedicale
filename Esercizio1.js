0a78977b54d3c023615c00c1a4522cb05ac33499
carlocriscenti <carlocriscenti05@gmail.com>
2025-05-15 12:54:41 +0200

    rimosso il file Esercizio1

--- Esercizio1.js
+++
@@ -1,5 +0,0 @@
-var nome = "Carlo";
-var eta = 20;
-var isStudente = true;
-var hobby = ["palestra", "camminare"];
-console.log("Ciao, mi chiamo ".concat(nome, ", ho ").concat(eta, " anni e i miei hobby sono: ").concat(hobby));
